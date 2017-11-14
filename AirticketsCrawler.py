cdb6d5bbdb1e0a4c631ec93364b7c1a40223a4c4
Alan7305 <33473357+Alan7305@users.noreply.github.com>
2017-11-14 18:09:47 +0800

    new

--- AirticketsCrawler.py
+++ AirticketsCrawler.py
@@ -1,9 +1,17 @@
 ﻿from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Chrome("C:/Users/Alan/Downloads/chromedriver_win32/chromedriver.exe")
-browser.get('https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1510133860&rver=6.7.6640.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3frealm%3dmsn.com%26RpsCsrfState%3d83913b4a-51a3-43ab-a6fe-d0448192a6f4&id=292841&whr=msn.com&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=90015#wa=wsignin1.0')
+options = webdriver.ChromeOptions()
 
+# 设置中文
+#options.add_argument('lang=zh_CN.UTF-8')
+# 更换头部
+aaoptions.add_argument('user-agent="Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like Mac OS X; ja-jp) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5F137 Safari/525.20"')
+
+browser = webdriver.Chrome("C:/Users/Alan/Downloads/chromedriver_win32/chromedriver.exe",chrome_options=options)
+browser.get('https://www.skyscanner.com.tw/transport/d/tpe/2018-04-07/kix/tak/2018-04-14/tpe?adults=2&children=1&adultsv2=2&childrenv2=5&infants=0&cabinclass=economy&ref=day-view&seo_airline=dd#results')
+
+"""
 element = browser.find_element_by_id("i0116")
 element.send_keys("hddsom")
 element = browser.find_element_by_id("idSIButton9")
@@ -12,3 +20,4 @@ element = browser.find_element_by_id("i0118")
 element.send_keys("@XXXXXXXX")
 element = browser.find_element_by_id("idBtn_Back")
 element.send_keys(Keys.RETURN)
+"""
