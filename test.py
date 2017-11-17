9ed7b298ccc2dd15dce52660161b578e232c88b7
Alan7305 <33473357+Alan7305@users.noreply.github.com>
2017-11-17 11:04:39 +0800

    ㄇ

--- test.py
+++ test.py
@@ -9,25 +9,3 @@ AC = AirticketsCrawler.Quote('2018-04-07', '2018-04-14', 2, 1, 'tpe', 'kix', 'ta
 AC.export('json_Airtickets/Airtickets_'+datetime.datetime.now().strftime('%Y%m%d')+'.json')
 
 print(AC.data)
-
-
-
-def hello(_value):
-    """print word"""
-    print("hello " + _value + "!")
-
-hello("world")
-
-print(2)
-
-str_1 = '17:10TPE2 小時 55 分鐘直飛 21:05KIX 19:05TAK2 小時 40 分鐘直飛 20:45TPE'
-str_2 = re.search(r"\d{2}:\d{2}", str_1).group().replace(',','')
-print(str_2)
-
-
-data = {}
-data['a']=[]
-data['a'].append({'a':'a'})
-
-with open('test.txt', 'w') as f:
-  json.dump(data, f, ensure_ascii=False)
